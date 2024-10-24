2dfd148ebaedabee38150d2e2c0b045961e77713
stanmelchizedek <stanexseven@gmail.com>
2024-10-24 14:21:04 +0300

    add shadcn

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,14 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+    darkMode: ["class"],
+    content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
   theme: {
-    extend: {
-      colors: {
-        darkForestGreen: "#2F3A2F",
-        forestGreen: "#2F682E",
-        primaryBrown: "#8B5A2B",
-      },
-    },
+  	extend: {
+  		colors: {
+  			darkForestGreen: '#2F3A2F',
+  			forestGreen: '#2F682E',
+  			primaryBrown: '#8B5A2B'
+  		},
+  		borderRadius: {
+  			lg: 'var(--radius)',
+  			md: 'calc(var(--radius) - 2px)',
+  			sm: 'calc(var(--radius) - 4px)'
+  		}
+  	}
   },
-  plugins: [],
+  plugins: [require("tailwindcss-animate")],
 };
